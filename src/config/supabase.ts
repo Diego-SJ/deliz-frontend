9521d5ed4d17d0036d1a516720eeb86f6bcf171c
Diego Salas <107061438+jd-sj@users.noreply.github.com>
2024-04-22 12:30:25 -0600

    development environment

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  'https://maavzvodgeradikmoehp.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hYXZ6dm9kZ2VyYWRpa21vZWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU0MjU4NDQsImV4cCI6MjAwMTAwMTg0NH0.kEZqTHJjo_ayqbEhaXd6Gs8GaSwzbTb0t_s0EqxFim0',
+  'https://onvvqhjemwngjsxvqxnk.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9udnZxaGplbXduZ2pzeHZxeG5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM4MDk0MjUsImV4cCI6MjAyOTM4NTQyNX0.32moi2gcZqRPK977GKPlzUjdYBor52eGlOifrVuGf5w',
 );
